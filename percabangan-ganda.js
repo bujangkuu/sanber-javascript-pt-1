3e1c86cbe983c4f7e3ec348910461e8a961732d3
Nama Anda <email@g.com>
2024-04-28 09:54:50 +0700

    Aplikasi Penjumlahan

---
+++ percabangan-ganda.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')({ sigint: true });
+//Percabangan lebih dari satu kasus (if else if else)
+const nilai = prompt('Masukan nilai: ');
+var grade;
+
+if (nilai > 90) {
+    grade = "A"
+} else if (nilai > 80) {
+    grade = "B+"
+} else if (nilai > 70) {
+    grade = "B"
+} else {
+    grade = "F"
+}
+console.log("Grade adalah = " + grade)
--- percabangan.js
+++ percabangan.js
@@ -3,7 +3,7 @@ const prompt = require('prompt-sync')({ sigint: true });
 const password = prompt('Enter password: ');
 const validPassword = 'password'
 
-// perbandingan
+// perbandingan password (string) vs valiPassword(string)
 if (password === validPassword) {
     //jika benar
     console.log("Selamat datang bos!")
@@ -11,4 +11,20 @@ if (password === validPassword) {
     //jika salah
     console.log("Password Salah, coba lagi!")
 }
+// print ini apapun yang terjadi
 console.log("Terimakasih sudah menggunakan aplikasi kami")
+const prompt = require('prompt-sync')({ sigint: true });
+
+const nilai = prompt('Masukan nilai: ');
+var grade;
+
+if (nilai > 90) {
+    grade = "A"
+} else if (nilai > 80) {
+    grade = "B+"
+} else if (nilai > 70) {
+    grade = "B"
+} else {
+    grade = "F"
+}
+console.log("Grade adalah = " + grade)
